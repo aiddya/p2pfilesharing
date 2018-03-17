06e673576b4f3275685d7db4dcd3ba2f0d3c798a
Athul Iddya <git@iddya.com>
2018-03-17 19:29:20 -0400

    Stub MessageHandler

--- src/cnp2p/MessageHandler.java
+++ src/cnp2p/MessageHandler.java
@@ -1,4 +1,27 @@
 package cnp2p;
 
-public class MessageHandler {
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class MessageHandler extends Thread {
+    Socket connection;
+    ObjectInputStream inputStream;
+    ObjectOutputStream outputStream;
+    int connIndex;
+
+    MessageHandler(Socket connection, int connIndex) {
+        this.connection = connection;
+        this.connIndex = connIndex;
+    }
+
+    public void run() {
+        try {
+            outputStream = new ObjectOutputStream(connection.getOutputStream());
+            outputStream.flush();
+            inputStream = new ObjectInputStream(connection.getInputStream());
+        } catch (Exception e) {
+            
+        }
+    }
 }
