7741c7a1209736129c11e61d95b6129d1a0a59ab
Vivek Chintalapati <vivek@yousee.in>
2018-04-18 00:58:03 -0400

    Choke, unchoke and optimistically unchoke

--- src/cnp2p/Main.java
+++ src/cnp2p/Main.java
@@ -4,9 +4,15 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.CopyOnWriteArrayList;
-
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+import cnp2p.ChokeStatus;
 public class Main {
     private static List<ConnectionHandler> connectionHandlerList;
 
@@ -84,5 +90,51 @@ public class Main {
                 System.out.println("Failed to initiate connection with " + peer.getHostName());
             }
         }
+
+
+        Thread unchokingThread = new Thread(() -> {
+            try {
+                while (true) {
+            			Collections.sort(connectionHandlerList, Collections.reverseOrder(Comparator.comparingDouble(ConnectionHandler :: getDownloadRate)));
+            			int i=0;
+            			for(ConnectionHandler preferred : connectionHandlerList) {
+            				if((preferred.getPeerStatus() == ChokeStatus.CHOKED || preferred.getPeerStatus() == null || preferred.getPeerStatus() == ChokeStatus.OPT_UNCHOKED) && i < Config.getInstance().getPreferredNeighbors()) {
+	                        Message unchoke = new Message(MessageType.UNCHOKE);
+	                        preferred.addMessage(unchoke);
+            				}
+            				i++;
+            			}
+            			for(int j= Config.getInstance().getPreferredNeighbors(); j < connectionHandlerList.size(); j++) {
+            				if(connectionHandlerList.get(j).getMyStatus() != ChokeStatus.OPT_UNCHOKED) {
+	                        Message choke = new Message(MessageType.CHOKE);
+	                        connectionHandlerList.get(j).addMessage(choke);
+            				}
+            			}
+            			Thread.sleep(Config.getInstance().getUnchokingInterval() * 1000);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        unchokingThread.start();
+        
+        Thread opt_unchokingThread = new Thread(() -> {
+            try {
+                while (true) {
+                		Message opt_unchoke = new Message(MessageType.UNCHOKE);
+            			ConnectionHandler optUnchokedHandler = connectionHandlerList.get(ThreadLocalRandom.current().nextInt(Config.getInstance().getPreferredNeighbors(), connectionHandlerList.size()));
+            			optUnchokedHandler.addMessage(opt_unchoke);
+            			optUnchokedHandler.setMyStatus(ChokeStatus.OPT_UNCHOKED);
+            			Thread.sleep(Config.getInstance().getOptimisticUnchokingInterval() * 1000);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        opt_unchokingThread.start();
+        
+        
     }
 }
